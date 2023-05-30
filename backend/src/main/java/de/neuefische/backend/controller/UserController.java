0de6f5329d9724a50630ce622de9a247f6d32895
Dominic <andro5@web.de>
2023-05-30 15:26:15 +0200

    add UserController and Argon2PWEncoder

---
+++ backend/src/main/java/de/neuefische/backend/controller/UserController.java
@@ -0,0 +1,35 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.service.MongoUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final MongoUserDetailsService service;
+
+    @GetMapping("/me")
+    public String getMeControllerOnly(Principal principal) {
+        if (principal != null) {
+            return principal.getName();
+        }
+        return "Du bist nicht eingeloggt!";
+    }
+
+    @GetMapping("/me2")
+    public String getMeFromEverywhere(){
+        System.out.println(SecurityContextHolder.getContext());
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+    }
+}
--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,7 +18,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
     @Bean
@@ -27,7 +28,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests()
                 .requestMatchers("/api/cars").authenticated()
-                .requestMatchers("/user/me").permitAll()
+                .requestMatchers("/user/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
